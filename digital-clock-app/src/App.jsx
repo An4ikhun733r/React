46f4136e8df7059a89eeeb2cbfd3fe577e8b62fb
An4ikhun733r <anuar.ospanov.prof@gmail.com>
2024-05-10 19:57:57 +0500

    digital-clock-app

---
+++ digital-clock-app/src/App.jsx
@@ -0,0 +1,11 @@
+import DigitalClock from "./DigitalClock";
+
+function App(){
+    return (<>
+        <DigitalClock />
+        <DigitalClock />
+        <DigitalClock />
+    </>);
+}
+
+export default App;
